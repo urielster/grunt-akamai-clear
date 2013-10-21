17bebb5655b6d2ac97e63bb9f28547d3220d7e44
urielster <uriel.elster@gmail.com>
2013-10-21 21:22:24 +0300

    pattern invaldiation
    Add a feature to match a pattern and get the file list for future
    invalidation, in other this is how i can invalidate a given folder of
    files in akamai (it could be easier if i could invalidate using
    wildcard in akamai)

--- tasks/akamai-clear.js
+++ tasks/akamai-clear.js
@@ -19,9 +19,13 @@ module.exports = function(grunt) {
   grunt.registerMultiTask('akamai', 'Purge/Invalidate Akamai', function() {
     var done = this.async();
     var data = this.data;
+    var matchers  = (data.fileMatch)? data.fileMatch : null ;
 
+    var urls = data.urls || getFoldersFileNames(matchers);
+
+    grunt.log.verbose.ok(urls);
     var thisPurge = new akamai.purge(
-      data.urls, {
+      urls, {
       user: data.user,
       password: data.password,
       domain: (data.domain || 'production'),
@@ -35,5 +39,29 @@ module.exports = function(grunt) {
       done('akamai request sent');
     });
   });
+
+  /**
+   * Let it search fs and get the list of files to invalidate
+   * @param fileMatch
+   * @returns {Array}
+   */
+  var getFoldersFileNames = function(fileMatch){
+
+    var resultUrls = [];
+
+    if (fileMatch && fileMatch !== null && fileMatch.pattern && fileMatch.dest){
+      var options = fileMatch.options;
+      options.rename = function(dest, matchedSrcPath, options) {
+        return (dest +  matchedSrcPath); //instead of using path which is for real paths in fs
+      };
+
+      var map = grunt.file.expandMapping(fileMatch.pattern, fileMatch.dest,fileMatch.options );
+      map.forEach(function(url){
+        resultUrls.push(url.dest);
+      });
+    }
+    return resultUrls;
+
+  }
 
 };
